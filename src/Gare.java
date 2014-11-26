594b4ceebb953cd520d2bcc46c52142c714b9fdc
anne-sophie-balestra <anne.sophie.balestra@gmail.com>
2014-11-26 14:47:55 +0100

    V2 clean avec doc et diagramme de classes

--- src/Gare.java
+++ src/Gare.java
@@ -1,36 +1,51 @@
 import java.util.Random;
 
 public class Gare {
-	
-	//Constantes
-	static final int NB_TRAINS = 3;
-	static final int NB_VOYAGEURS = 25;
+   /*
+	* Constantes
+	*/
+	//Nombre de trains à créer
+	static final int NB_TRAINS = 5;
+	//Nombre de voyageurs à créer
+	static final int NB_VOYAGEURS = 1000;
+	//Nombre de voies sur le quai
 	static final int NB_VOIES = 2;
+	//Nombre de guichets dans la billeterie
 	static final int NB_GUICHET = 2;
-	static final int CAPACITE_TRAIN = 20;
+	//Capacité maximum du plus grand train
+	static final int CAPACITE_TRAIN = 100;
+	//Temps d'arret en gare des trains
 	static final int ARRET_TRAIN = 1000;
 	
-	//Variables
+	/*
+	* Variables
+	*/
+	//Tableau contenant tous les trains créés
 	private Train[] trains = new Train[NB_TRAINS];
+	//Tableau contenant tous les voyageurs créés
 	private Voyageur[] voyageurs = new Voyageur[NB_VOYAGEURS];
+	//Création d'une billeterie pour la gare
 	private Billeterie billeterie;
+	//Création d'un quai pour la gare
 	private Quai quai;
+	//Nombre de voyageurs créés
 	private int nbVoyageurs = 0;
+	//Nombre de trains créés
 	private int nbTrains = 0;
 	
 	/**
 	 * Création d'un voyageur
-	 * @return
+	 * @return true si la création s'est bien passée
 	 */
 	private boolean nouveauVoyageur() {
 		
-		//Si le nombre de voyageurs à atteint le nombre total de voyageurs alors on n'en créer pas de nouveau
+		//Si le nombre de voyageurs a atteint le nombre total de voyageurs alors on arrete d'en créer
 		if(nbVoyageurs == NB_VOYAGEURS) {
 			System.out.println("Le nombre maximum de voyageurs est atteint.");
 			return false;
 		}
 		
-		//Sinon onn créer un nouveau voyageur
+		//Sinon on créé un nouveau voyageur
 		voyageurs[nbVoyageurs] = new Voyageur(quai, billeterie);
 		nbVoyageurs++;
 		return true;
@@ -38,18 +53,18 @@ public class Gare {
 	
 	/**
 	 * Création d'un train
-	 * @return
+	 * @return true si la création s'est bien passée
 	 */
 	private boolean nouveauTrain() {
 		
-		//Si le nombre de trains à atteint le nombre total de trains alors on n'en créer pas de nouveau
+		//Si le nombre de trains a atteint le nombre total de trains alors on arrete d'en créer
 		if(nbTrains == NB_TRAINS) {
 			System.out.println("Le nombre maximum de trains est atteint.");
 			return false;
 		}
 		
-		//Sinon on créer un nouveau train
-		//Avec une capacité et une vitesse de transport aléatoire
+		//Sinon on créé un nouveau train
+		//Avec une capacité et une vitesse de transport aléatoires
 		Random rand = new Random();
 		int vitesse = Math.abs(rand.nextInt((300 - 50) + 1) + 50);
 		int capacite = Math.abs(rand.nextInt(CAPACITE_TRAIN));
@@ -69,8 +84,6 @@ public class Gare {
 		//Création quai (int nbvoie)
 		this.quai = new Quai(NB_VOIES, this.billeterie);
 	
-		
-		
 		//Création de tous les voyageurs
 		while(nouveauVoyageur());
 		
@@ -82,7 +95,7 @@ public class Gare {
 			trains[i].start();
 		}
 		
-		//Démarage de tous les threads voyageurs avec un temps d'attente de 10s entre chaque voyageur
+		//Démarage de tous les threads voyageurs avec un temps d'attente de 10ms entre chaque voyageur
 		for(int i = 0;i < nbVoyageurs; i++){
 			voyageurs[i].start();
 			try{
@@ -93,8 +106,6 @@ public class Gare {
 	
 	//Fonction de lancement d'une gare (main).
 	public static void main(String[] args) {
-
-		new Gare();
-		
+		new Gare();		
 	}
 }
--- src/Voyageur.java
+++ src/Voyageur.java
@@ -1,6 +1,8 @@
 public class Voyageur extends Thread{
 
+	//quai ou le voyageur va prendre son train
 	private Quai quai;
+	//billeterie ou le voyageur achete son train
 	private Billeterie billeterie;
 	
 	/**
@@ -17,13 +19,12 @@ public class Voyageur extends Thread{
 	 * Plan d'execution
 	 * Voyageur
 	 * 1) Achete un billet
-	 * 2) Monte dans un train NON PLEINS des que possible
+	 * 2) Monte dans le train indiqué sur le billet
 	 */
 	public void run() {
 		//Va acheter un billet puis récupére la voie associé au billet acheté
 		int voie = billeterie.vendreBillet();
 		//Monte dans le train sur la voie indiquée sur le billet
 		quai.monterTrain(voie);
-	}
-	
+	}	
 }
